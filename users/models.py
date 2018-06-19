83677542fa3fda2c507f1056f9aaa56ef3a11b03
Tiago Costa <tiago.costa@mesainc.com.br>
2018-06-19 15:25:55 -0300

    Added basic configuration

--- users/models.py
+++ users/models.py
@@ -8,17 +8,9 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin, IndexedTimeStampedModel):
-    email = models.EmailField(max_length=255, unique=True)
-    is_staff = models.BooleanField(
-        default=False,
-        help_text=_('Designates whether the user can log into this admin '
-                    'site.'))
-    is_active = models.BooleanField(
-        default=True,
-        help_text=_('Designates whether this user should be treated as '
-                    'active. Unselect this instead of deleting accounts.'))
+    name = models.CharField(max_length=255)
 
-    objects = UserManager()
+    # objects = UserManager()
 
     USERNAME_FIELD = 'email'
 
