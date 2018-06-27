23068adc78c35d4b3e31e7789d18f3a6c7a7b115
Tiago Costa <tiago.costa@mesainc.com.br>
2018-06-27 11:22:29 -0300

    Style commits list

--- staticfiles/js/app/example-app/components/AppContent/AppContent.js
+++ staticfiles/js/app/example-app/components/AppContent/AppContent.js
@@ -24,11 +24,29 @@ class AppContent extends React.Component {
               <img src={"/static/images/addRepoImage.png"} srcSet={`${"/static/images/addRepoImage@2x.png"} 2x`} />
             </div>
           </div>
+          <div className="commits-table">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Commit ID (sha)</th>
+                  <th>Message</th>
+                  <th>Repository</th>
+                  <th>Author Name</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.commits.map(item => <tr key={item.id}>
+                  <th>{item.sha}</th>
+                  <th>{item.message}</th>
+                  <th>{item.repository.name}</th>
+                  <th>{item.github_author_name}</th>
+                  <th>{Moment(item.date).format('MM/DD/YYYY')}</th>
+                </tr>)}
+              </tbody>
+            </table>
+          </div>
 
-          <ul>
-            {this.props.commits.map(item => <li key={item.id}>
-              {item.sha}, {item.message}, {item.repository.name}, {item.github_author_name}, {Moment(item.date).format('MM/DD/YYYY')}</li>)}
-          </ul>
         </div>
       </div>
     );
